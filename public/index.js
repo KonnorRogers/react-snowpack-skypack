9b5de014e7ce5d3c76f1f69b34557226c0c73c6f
ParamagicDev <konnor5456@gmail.com>
2020-12-12 15:10:06 -0500

    remove node_modules and public

--- public/index.js
+++
@@ -1,8 +0,0 @@
-import React from "https://cdn.skypack.dev/react@^16.13.1";
-import ReactDOM from "https://cdn.skypack.dev/react-dom@^16.13.1";
-const App = () => {
-  return /* @__PURE__ */ React.createElement("div", {
-    className: "box"
-  }, "Whatever.");
-};
-ReactDOM.render(/* @__PURE__ */ React.createElement(App, null), document.getElementById("root"));
